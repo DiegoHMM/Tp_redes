64cc6cfa0ac4d55faf07e991ee92583d56fef248
Diego Henrique Marques Matos <diego.matos@aluno.ufop.edu.br>
2021-04-18 15:48:08 -0300

    diretorios organizados

--- Bat/Execute_cinco.bat
+++ Bat/Execute_cinco.bat
@@ -2,11 +2,8 @@ cd..
 start /min cmd /c python Server.py
 start /min cmd /c python Client.py
 start /min cmd /c python Client.py
-<<<<<<< HEAD:Bat/Execute_cinco.bat
-=======
 start /min cmd /c python Client.py
 start /min cmd /c python Client.py
 start /min cmd /c python Client.py
 start /min cmd /c python Client.py
->>>>>>> 565f5cee72e5cf57fd394069200fc8405a423dce:Execute_cinco.bat
 start /min cmd /c python Client.py modelo_cinco
