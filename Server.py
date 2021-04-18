18bbc46d6c71ba6513d1167bb0fd934c6100d53d
Guilherme Rocha <guitfrocha@gmail.com>
2021-04-18 12:29:29 -0300

    encerrando servidor

--- Server.py
+++ Server.py
@@ -1,19 +1,26 @@
 import socket
 import threading
 import time
+import sys
 
 class TcpServer(object):
 
     list_addr = []
     qnt_entradas = 0
     count_entradas = 0
+    exit_flag = False
+    closed = False
+    s = None
 
     def tcplink(self, sock, addr):
         while True:
+            if self.exit_flag:
+                              
+                break
             try:
                 data = sock.recv(1024)
 
-                time.sleep(1)
+                #time.sleep(0.5)
                 if data.decode("UTF-8").startswith('*'):
                     data = data.decode("UTF-8")
                     data = str(data).replace('*','')
@@ -47,7 +54,12 @@ class TcpServer(object):
                 if(self.qnt_entradas*2 == self.count_entradas):
                     exit_socket = "exit"
                     self.list_addr[0][0].send(exit_socket.encode("UTF-8"))
-                    break
+                    self.exit_flag = True
+                    if not self.closed:
+                        self.closed = True
+                        self.s.close()
+                        break
+                    
                 
             except ConnectionResetError as cre:
                 break
@@ -60,21 +72,31 @@ class TcpServer(object):
 
 
     def create_sokcet(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        s.bind(("localhost", 4001))
-        s.listen(2)
+        self.s.bind(("localhost", 4001))
+        self.s.listen(2)
         print("Waiting for connection...")
 
         while True:
-
-            sock, addr = s.accept()
-            print("seccessful connect!")
-            self.list_addr.append([sock,addr])
-            t = threading.Thread(target=self.tcplink, args=(sock, addr))
-            t.start()
+            
+            if self.exit_flag:
+                break
+            try:
+                sock, addr = self.s.accept()
+                print("successful connect!")
+                self.list_addr.append([sock,addr])
+                t = threading.Thread(target=self.tcplink, args=(sock, addr))
+                t.start()
+            except:
+                print("Encerrando Servidor! ...")
+            
+            
+            
+            
 
 
 if __name__ == '__main__':
     server = TcpServer()
     server.create_sokcet()
+    
