004536d2d7a82dfb0fba1ac7201e9c2aa11a4859
Diego Henrique Marques Matos <diego.matos@aluno.ufop.edu.br>
2021-04-17 22:39:34 -0300

    troca repositorio

---
+++ Server.py
@@ -0,0 +1,54 @@
+import socket
+import threading
+import time
+
+class TcpServer(object):
+
+    list_addr = []
+
+    def tcplink(self, sock, addr):
+        #print('Accept new connection from %s:%s...' % addr)
+        while True:
+            try:
+                data = sock.recv(1024)
+
+                time.sleep(1)
+                '''
+                if data:
+                    
+                    print("Recebendo do cliente:")
+                    print(data)
+                    print('----------------------')
+                    resp = str(data.decode("UTF-8"))
+                '''
+            
+                if data.decode("UTF-8").startswith('-'):
+                    self.list_addr[0][0].send(data)
+                elif data.decode("UTF-8").startswith('+'):
+                    self.list_addr[1][0].send(data)
+
+            except ConnectionResetError as cre:
+                print('error', cre)
+                break
+        sock.close()
+        print('Connection from %s:%s closed.' % addr)
+
+    def create_sokcet(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        s.bind(("localhost", 4001))
+        s.listen(2)
+        print("Waiting for connection...")
+
+        while True:
+
+            sock, addr = s.accept()
+            print("seccessful connect!")
+            self.list_addr.append([sock,addr])
+            t = threading.Thread(target=self.tcplink, args=(sock, addr))
+            t.start()
+
+
+if __name__ == '__main__':
+    server = TcpServer()
+    server.create_sokcet()
---
+++ main.py
@@ -0,0 +1,38 @@
+import re
+
+def contador_de_letras(palavra):
+    vogal = ['a','e','i','o','u']
+    consoante = ['q','w','r','t','y','p','s','d','f','g','h','j','k','l','ç','z','x','c','v','b','n','m']
+    numeros = ['0','1','2','3','4','5','6','7','8','9']
+    palavra = palavra.lower()
+
+    cont_vogal = 0
+    cont_consoante = 0
+    cont_numeros = 0
+    tam_palavra = len(palavra)
+    for i in palavra:
+        if i in vogal:
+            cont_vogal = cont_vogal + 1
+        elif i in consoante:
+            cont_consoante = cont_consoante + 1
+        elif i in numeros:
+            cont_numeros = cont_numeros + 1
+    return  palavra, cont_consoante, cont_vogal, cont_numeros
+
+
+def le_arquivo(nome_arquivo):
+    f = open(nome_arquivo+'.txt', "r")
+    l = []
+    for line in f:
+        
+        if(len(line) >= 2):
+            if line[0]+line[1] == '//':
+                print(" ")
+            elif line != '\n':
+                line = line.replace('\n','')
+                line = re.sub('[^a-zA-Z0-9\n\.]', ' ', line)
+                l.append(line)
+        elif line != '\n':
+            line = re.sub('[^a-zA-Z0-9\n\.]', ' ', line)
+            l.append(line)
+    return l
