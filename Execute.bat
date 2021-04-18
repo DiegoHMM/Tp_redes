1d66d9c1a9c64ccc461f89d079f0ba54f0f21eef
Diego Henrique Marques Matos <diego.matos@aluno.ufop.edu.br>
2021-04-18 13:11:14 -0300

    .bat unico

--- Execute.bat
+++
@@ -1,5 +0,0 @@
-start /min cmd /c Server.bat
-timeout /t 3
-start /min cmd /c Client1.bat
-timeout /t 3
-start /min cmd /c Client2.bat
---
+++ Execute_um.bat
@@ -0,0 +1,3 @@
+start /min cmd /c python Server.py
+start /min cmd /c python Client.py
+start /min cmd /c python Client.py modelo_entrada
